e0b5468b66994f1f44719ed76d37d1a695e24480
Sergio Torres <sdtorresl@unal.edu.co>
2017-07-31 23:08:40 -0500

    Formulario de contacto funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,55 +35,56 @@
 
         // Forms.
         var form = $('#ajax-contact');
-				// Set up an event listener for the contact form.
-				$(form).submit(function(event) {
-				    // Stop the browser from submitting the form.
-				    event.preventDefault();
-
-	            var formData = $(form).serialize();
-	            var formMessages = $("#form-messages");
-
-	            // Submit the form using AJAX.
-	            $.ajax({
-	                type: 'POST',
-	                url: $(form).attr('action'),
-	                data: formData
-	            }).done(function(response) {
-
-	                // Make sure that the formMessages div has the 'success' class.
-	                $(formMessages).removeClass('error');
-	                $(formMessages).addClass('success');
-
-	                // Set the message text.
-	                $(formMessages).text(response);
-
-	                // Clear the form.
-	                $('#name').val('');
-	                $('#email').val('');
-	                $('#message').val('');
-	            }).fail(function(data) {
-	                // Make sure that the formMessages div has the 'error' class.
-	                $(formMessages).removeClass('success');
-	                $(formMessages).addClass('error');
-
-	                // Set the message text.
-	                if (data.responseText !== '') {
-	                    $(formMessages).text(data.responseText);
-	                } else {
-	                    $(formMessages).text('Oops! An error occured and your message could not be sent.');
-	                }
-	            });
-				});
 
         // Hack: Activate non-input submits.
-        /*$('form').on('click', '.submit', function(event) {
+        $('form').on('click', '.submit', function(event) {
 					// Stop propagation, default.
-					//event.stopPropagation();
-					//event.preventDefault();
+					event.stopPropagation();
+					event.preventDefault();
 
 					// Submit form.
 					//$(this).parents('form').submit();
-        });*/
+
+          var formData = $(form).serialize();
+          var formMessages = $("#form-messages");
+
+          // Show loading
+          $(".lds-css").show();
+
+          // Submit the form using AJAX.
+          $.ajax({
+              type: 'POST',
+              url: $(form).attr('action'),
+              data: formData
+          }).done(function(response) {
+
+              // Make sure that the formMessages div has the 'success' class.
+              $(formMessages).removeClass('error');
+              $(formMessages).addClass('success');
+
+              // Set the message text.
+              $(formMessages).text(response);
+
+              // Clear the form.
+              $('#name').val('');
+              $('#email').val('');
+              $('#message').val('');
+
+          }).fail(function(data) {
+              // Make sure that the formMessages div has the 'error' class.
+              $(formMessages).removeClass('success');
+              $(formMessages).addClass('error');
+
+              // Set the message text.
+              if (data.responseText !== '') {
+                  $(formMessages).text(data.responseText);
+              } else {
+                  $(formMessages).text('Oops! An error occured and your message could not be sent.');
+              }
+          }).always(function (data) {
+            $(".lds-css").hide();
+          });
+        });
 
         // Prioritize "important" elements on medium.
         skel.on('+medium -medium', function() {
